211ff7845d8fca7ad0bbca7bae7da5e47a27b7d0
ShihShihDer <suc12345@gmail.com>
2022-10-31 23:37:47 +0800

    del PDF & readCSV From folder

--- src/ioEmp/TestIO.java
+++ src/ioEmp/TestIO.java
@@ -11,60 +11,67 @@ import org.xml.sax.SAXException;
 
 import java.io.*;
 import java.lang.management.MemoryType;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
+import java.util.Objects;
 
 public class TestIO {
-/*public static void findAllFilesInFolder(File folder){
-	for (File file:folder.listFiles()) {
-		if (!file.isDirectory()){
-			System.out.println(file.getName());
-			AutoDetectParser parser = new AutoDetectParser();
-			parser.setParsers(new HashMap<MemoryType, Parser>());
+    /*public static void findAllFilesInFolder(File folder){
+        for (File file:folder.listFiles()) {
+            if (!file.isDirectory()){
+                System.out.println(file.getName());
+                AutoDetectParser parser = new AutoDetectParser();
+                parser.setParsers(new HashMap<MemoryType, Parser>());
 
-			Metadata metadata = new Metadata();
-			metadata.add(TikaMetadataKeys);
-		}else {
-			findAllFilesInFolder(file);
-		}
-	}
-};*/
+                Metadata metadata = new Metadata();
+                metadata.add(TikaMetadataKeys);
+            }else {
+                findAllFilesInFolder(file);
+            }
+        }
+    };*/
 //	D:\javaSSD\project1\P1Data\111年9月2日候選人登記概況_csv
-public String TestTika() throws IOException {
-	File file = new File("D:\\javaSSD\\project1\\P1Data\\111年9月2日候選人登記概況_csv");
+/*public String TestTika() throws IOException {
+	File file = new File("..project1\\P1Data\\111年9月2日候選人登記概況_csv");
 	Tika tika = new Tika();
 	return tika.detect(file);
+}*/
+    public static void main(String[] args) {
+        File folder = new File("..\\project1\\P1Data\\111年9月2日候選人登記概況_csv");
+        Tika tika = new Tika();
+        for (File file : Objects.requireNonNull(folder.listFiles())) {
+            if (!file.isDirectory()) {
+                try {
+                    String fileType = tika.detect(file);
+                    if (fileType.equals("text/csv")) {
+                        try (FileInputStream fis = new FileInputStream(file);
+                             InputStreamReader isr = new InputStreamReader(fis, StandardCharsets.UTF_8);
+                             BufferedReader br = new BufferedReader(isr)
+                        ) {
+                            CSVFormat.Builder builder = CSVFormat.Builder.create();
+                            builder.setHeader("\"選舉區\"", "登記日期", "姓名", "推薦之政黨", "備註");
+                            builder.setSkipHeaderRecord(true);
+                            Iterable<CSVRecord> records = builder.build().parse(br);
+                            for (CSVRecord record : records) {
+                                System.out.println(record.get("\"選舉區\"").replace(",","")+","+
+                                        record.get("登記日期").replace(",","")+","+
+                                        record.get("姓名").replace(",","")+","+
+                                        record.get("推薦之政黨").replace(",","")+","+
+                                        record.get("備註").replace(",",""));
+                            }
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
 
-}
-	public static void main(String[] args) {
-		File file = new File("D:\\javaSSD\\project1\\P1Data\\111年9月2日候選人登記概況_csv\\1_2-111年直轄市長選舉候選人登記彙總表.csv");
-		Tika tika = new Tika();
-		try {
-			String a = tika.detect(file);
-			System.out.println(a);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
+                    }
+                    System.out.println(file.getName());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+        }
+    }
 
 
-//		try (FileInputStream fis = new FileInputStream("/Users/vincent/Downloads/Employees.csv");
-//			 InputStreamReader isr = new InputStreamReader(fis, "ms950");
-//			 BufferedReader br = new BufferedReader(isr)
-//		) {
-//			CSVFormat.Builder builder = CSVFormat.Builder.create();
-//			builder.setHeader("員工編號","姓名","薪水","年齡");
-//			builder.setSkipHeaderRecord(true);
-//			Iterable<CSVRecord> records = builder.build().parse(br);
-//			int sum = 0;
-//			for (CSVRecord record : records) {
-//				String salary = record.get("薪水").replace(",","");
-//				int sal = Integer.parseInt(salary);
-//				sum += sal;
-//			}
-//			System.out.println("Sum = "+sum);
-//
-//		} catch (IOException e) {
-//			throw new RuntimeException(e);
-//		}
-
-	}
 }
